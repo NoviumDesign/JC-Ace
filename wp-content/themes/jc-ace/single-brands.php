b6f78e832822d07a8564e38d95ee1ba60dbb55dd
Samuel Lie <samuellie@Samuels-MacBook-Pro.local>
2015-11-04 16:06:36 +0100

    Small design fixes

--- wp-content/themes/jc-ace/single-brands.php
+++ wp-content/themes/jc-ace/single-brands.php
@@ -14,156 +14,156 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+      <?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header min-height-100" style="background-image: url('<?php echo get_field( "brand-top-image" ); ?>')">
-						<div class="container">
-			        <div class="valign-wrapper min-height-100">
-			          <div class="valign height-100">					
-									<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-									<?php
-										edit_post_link(
-											sprintf(
-												/* translators: %s: Name of current post */
-												esc_html__( 'Edit this brand %s', 'jc-ace' ),
-												the_title( '<span class="screen-reader-text">"', '"</span>', false )
-											),
-											'<span class="edit-link">',
-											'</span>'
-										);
-									?>
-								</div>
-							</div>
-			        <div class="brand-intro">
-				      	<?php the_excerpt(); ?> 
-			        </div>
-						</div>
-					</header><!-- .entry-header -->
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header min-height-100" style="background-image: url('<?php echo get_field( "brand-top-image" ); ?>')">
+            <div class="container">
+              <div class="valign-wrapper min-height-100">
+                <div class="valign height-100">         
+                  <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                  <?php
+                    edit_post_link(
+                      sprintf(
+                        /* translators: %s: Name of current post */
+                        esc_html__( 'Edit this brand %s', 'jc-ace' ),
+                        the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                      ),
+                      '<span class="edit-link">',
+                      '</span>'
+                    );
+                  ?>
+                </div>
+              </div>
+              <div class="brand-intro">
+                <?php the_excerpt(); ?> 
+              </div>
+            </div>
+          </header><!-- .entry-header -->
 
-					<div class="entry-content">
+          <div class="entry-content">
 
-						<div class="single-brand-content container">
+            <div class="single-brand-content container">
 
-							<div class="brand-text">
-								<?php the_content(); ?>
-								<?php
-									wp_link_pages( array(
-										'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jc-ace' ),
-										'after'  => '</div>',
-									) );
-								?>
-							</div>
+              <div class="brand-text">
+                <?php the_content(); ?>
+                <?php
+                  wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jc-ace' ),
+                    'after'  => '</div>',
+                  ) );
+                ?>
+              </div>
 
-							<div class="image-container">
+              <div class="image-container">
 
-								<!-- First brand image -->
-								<?php 
+                <!-- First brand image -->
+                <?php 
 
-									$first_image = get_field('brand_image_1');
+                  $first_image = get_field('brand_image_1');
 
-								if( !empty($first_image) ): ?>
+                if( !empty($first_image) ): ?>
 
-									<div class="image-column">
-										<img src="<?php echo $first_image['url']; ?>" alt="<?php echo $first_image['alt']; ?>" />
-									</div>
+                  <div class="image-column">
+                    <img src="<?php echo $first_image['url']; ?>" alt="<?php echo $first_image['alt']; ?>" />
+                  </div>
 
-								<?php endif; ?>
+                <?php endif; ?>
 
-								<!-- second brand image -->
-								<?php 
+                <!-- second brand image -->
+                <?php 
 
-									$second_image = get_field('brand_image_2');
+                  $second_image = get_field('brand_image_2');
 
-								if( !empty($second_image) ): ?>
+                if( !empty($second_image) ): ?>
 
-									<div class="image-column">
-										<img src="<?php echo $second_image['url']; ?>" alt="<?php echo $second_image['alt']; ?>" />
-									</div>
+                  <div class="image-column">
+                    <img src="<?php echo $second_image['url']; ?>" alt="<?php echo $second_image['alt']; ?>" />
+                  </div>
 
-								<?php endif; ?>
+                <?php endif; ?>
 
-								<!-- third brand image -->
-								<?php 
+                <!-- third brand image -->
+                <?php 
 
-									$third_image = get_field('brand_image_3');
+                  $third_image = get_field('brand_image_3');
 
-								if( !empty($third_image) ): ?>
+                if( !empty($third_image) ): ?>
 
-									<div class="image-full-width">
-										<img src="<?php echo $third_image['url']; ?>" alt="<?php echo $third_image['alt']; ?>" />
-									</div>
+                  <div class="image-full-width">
+                    <img src="<?php echo $third_image['url']; ?>" alt="<?php echo $third_image['alt']; ?>" />
+                  </div>
 
-								<?php endif; ?>
+                <?php endif; ?>
 
-							</div>
+              </div>
 
-							<!-- old version
-							<?php 
+              <!-- old version
+              <?php 
 
-								$first_image = get_field('brand_image_1');
+                $first_image = get_field('brand_image_1');
 
-							if( !empty($first_image) ): ?>
+              if( !empty($first_image) ): ?>
 
-								<div class="content-box">
-									<img src="<?php echo $first_image['url']; ?>" alt="<?php echo $first_image['alt']; ?>" />
-								</div>
+                <div class="content-box">
+                  <img src="<?php echo $first_image['url']; ?>" alt="<?php echo $first_image['alt']; ?>" />
+                </div>
 
-							<?php endif; ?>
+              <?php endif; ?>
 
-							
-							<div class="content-box brand-text">
-								<?php the_content(); ?>
-								<?php
-									wp_link_pages( array(
-										'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jc-ace' ),
-										'after'  => '</div>',
-									) );
-								?>
-							</div>
-							
-							
-							<?php 
+              
+              <div class="content-box brand-text">
+                <?php the_content(); ?>
+                <?php
+                  wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jc-ace' ),
+                    'after'  => '</div>',
+                  ) );
+                ?>
+              </div>
+              
+              
+              <?php 
 
-								$second_image = get_field('brand_image_2');
+                $second_image = get_field('brand_image_2');
 
-							if( !empty($second_image) ): ?>
+              if( !empty($second_image) ): ?>
 
-								<div class="content-box">
-									<img src="<?php echo $second_image['url']; ?>" alt="<?php echo $second_image['alt']; ?>" />
-								</div>
+                <div class="content-box">
+                  <img src="<?php echo $second_image['url']; ?>" alt="<?php echo $second_image['alt']; ?>" />
+                </div>
 
-							<?php endif; ?>
-							
-							
-							<?php 
+              <?php endif; ?>
+              
+              
+              <?php 
 
-								$third_image = get_field('brand_image_3');
+                $third_image = get_field('brand_image_3');
 
-							if( !empty($third_image) ): ?>
+              if( !empty($third_image) ): ?>
 
-								<div class="content-box">
-									<img src="<?php echo $third_image['url']; ?>" alt="<?php echo $third_image['alt']; ?>" />
-								</div>
+                <div class="content-box">
+                  <img src="<?php echo $third_image['url']; ?>" alt="<?php echo $third_image['alt']; ?>" />
+                </div>
 
-							<?php endif; ?>
+              <?php endif; ?>
 
-							-->
+              -->
 
-						</div>
+            </div>
 
-					</div><!-- .entry-content -->
+          </div><!-- .entry-content -->
 
-				</article><!-- #post-## -->
+        </article><!-- #post-## -->
 
 
-			<?php endwhile; // End of the loop. ?>
+      <?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+  </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
